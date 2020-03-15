a9815e91f9827f8bda191b343343e84a27f6b7c0
prasoona <prasoona@caratred.com>
2020-03-15 16:23:36 +0530

    written functionality

---
+++ src/items.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  DropdownButton,
+  Dropdown
+} from "react-bootstrap";
+import data from "./data.json";
+const textColor = {
+  color: "black"
+};
+
+class Items extends Component {
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const url = "https://api.exchangeratesapi.io/latest?base=INR";
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    this.setState({ currencyRates: jsonResponse.rates, loading: false ,currency : 'INR'});
+    console.log(this.state.currencyRates);
+  }
+  onClickHandler = event => {
+    const value = event.target.innerHTML;
+    const cost = this.state.currencyRates[value]
+    this.setState({ currency : value })
+    data.forEach((each) =>{
+        console.log(each.cost, cost)
+        each.cost = each.cost * cost;
+        each.currency = value;
+    })
+  };
+  render() {
+    // const products = data.productsList;
+    // console.log(this.state.currencyRates)
+    return (
+      <React.Fragment>
+        <Container>
+          <Row>
+            {data.map(each => {
+              //   console.log(url);
+              return (
+                <Col key={each.id}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img key={each.id} variant="top" src={each.image} />
+                    <Card.Body>
+                      <Card.Text style={textColor}>
+                        {each.cost} {each.currency}
+                      </Card.Text>
+                      {/* <Button variant="primary">Go somewhere</Button> */}
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+
+            <Col>
+              {!this.state || this.state.loading ? (
+                <div>loading......</div>
+              ) : (
+                (console.log(this.state.currencyRates),
+                (
+                  <Dropdown style={{ marginTop: "1rem" }}>
+                    <Dropdown.Toggle id="dropdown-basic">
+                      {this.state && this.state.currency ? this.state.currency: 'Currency'}
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      {Object.keys(this.state.currencyRates).map(each => {
+                        return (
+                          <Dropdown.Item
+                            key={each}
+                            
+                            onClick={this.onClickHandler}
+                          >
+                            {each}
+                          </Dropdown.Item>
+                        );
+                      })}
+                    </Dropdown.Menu>
+                  </Dropdown>
+                ))
+              )}
+            </Col>
+          </Row>
+        </Container>
+        {/* <nav className="navbar navbar-dark bg-dark mb-3">
+            <a className="navbar-brand" href="#">
+              <h1>Total Items <span className="badge badge-secondary">{this.props.totalItems}</span></h1>
+            </a>
+          </nav> */}
+      </React.Fragment>
+    );
+  }
+}
+
+export default Items;
